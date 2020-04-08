71d3a6eee2482b2532dc22132314e0587f34070c
panicbit <panicbit.dev@gmail.com>
2020-04-08 19:43:29 +0200

    write json data to disk

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde_json as json;
 use common::*;
+use std::fs;
 
 mod bugs;
 mod fossils;
@@ -9,21 +10,24 @@ mod common;
 const IMAGE_DL_FOLDER: &str = "images";
 
 fn main() {
+    fs::create_dir_all("data").unwrap();
+
     // ### Bugs ###
     let bugs = bugs::fetch_all().expect("bugs");
     let json_bugs = json::to_string_pretty(&bugs).unwrap();
-    println!("{}", json_bugs);
-    download_images(bugs, IMAGE_DL_FOLDER).unwrap();
+    fs::write("data/bugs.json", json_bugs).unwrap();
 
     // ### Fish ###
     let fish = fish::fetch_all().expect("fish");
     let json_fish = json::to_string_pretty(&fish).unwrap();
-    println!("{}", json_fish);
-    download_images(fish, IMAGE_DL_FOLDER).unwrap();
+    fs::write("data/fish.json", json_fish).unwrap();
 
     // ### Fossils ###
     let fossils = fossils::fetch_all().expect("fossils");
     let json_fossils = json::to_string_pretty(&fossils).unwrap();
-    println!("{}", json_fossils);
+    fs::write("data/fossils.json", json_fossils).unwrap();
+
+    download_images(bugs, IMAGE_DL_FOLDER).unwrap();
+    download_images(fish, IMAGE_DL_FOLDER).unwrap();
     download_images(fossils, IMAGE_DL_FOLDER).unwrap();
 }
